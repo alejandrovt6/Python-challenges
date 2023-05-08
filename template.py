d51806b4d472448d13e7f45e34922fed1d73c911
Alejandro Vives Torres <alejandrovt1996@gmail.com>
2023-05-08 20:05:27 +0200

    0805

---
+++ template.py
@@ -0,0 +1,39 @@
+# Libraries
+
+
+# Program
+
+
+# GUI
+# window = tk.Tk()
+# window.title("Operations")
+# window.geometry("400x150")
+
+# screen_width = window.winfo_screenwidth()
+# screen_height = window.winfo_screenheight()
+
+# x = int((screen_width - window.winfo_screenmmwidth()) / 2)
+# y = int((screen_height - window.winfo_screenmmheight()) / 2)
+
+# window.geometry("+{}+{}".format(x, y))
+
+# input_label1 = tk.Label(window, text="Number 1:", padx=10, pady=5)
+# input_label1.grid(row=0, column=0)
+
+# entry1 = tk.Entry(window)
+# entry1.grid(row=0, column=1)
+
+# input_label2 = tk.Label(window, text="Number 2:", padx=10, pady=5)
+# input_label2.grid(row=1, column=0)
+
+# entry2 = tk.Entry(window)
+# entry2.grid(row=1, column=1)
+
+# button = tk.Button(window, text="Operate", command=operations)
+# button.grid(row=2, column=0)
+
+# result_label = tk.Label(window, text="", padx=10, pady=5)
+# result_label.grid(row=2, column=1)
+
+
+# window.mainloop()
