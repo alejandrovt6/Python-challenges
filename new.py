8daaace7fc210def1cde16b0d35599a0b3d5cdef
Alejandro Vives Torres <alejandrovt1996@gmail.com>
2023-06-06 18:59:02 +0200

    bank

--- new.py
+++ new.py
@@ -1,15 +1,7 @@
-# TODO: 
-# change windows size
-# display windows in the middle of the screen
-# add icons
-# calculate lendings
-
-# Libraries
 import tkinter as tk
 from tkinter import messagebox
 
-# Program
-class BankTeller:
+class BankTellerGUI:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Bank Teller")
@@ -21,8 +13,7 @@ class BankTeller:
 
     def create_widgets(self):
         # Secret Code Entry
-        secret_code_label = tk.Label(self.window, text="Enter your secret code: (0000)")
-
+        secret_code_label = tk.Label(self.window, text="Enter your secret code:")
         secret_code_label.pack()
 
         secret_code_entry = tk.Entry(self.window, textvariable=self.secret_code, show="*")
@@ -35,7 +26,7 @@ class BankTeller:
 
     def identification(self):
         code = self.secret_code.get()
-        if code == "0000":
+        if code == "1111":
             self.show_menu()
         else:
             messagebox.showerror("Identification", "Wrong code. Please try again.")
@@ -48,13 +39,13 @@ class BankTeller:
         show_cash_button = tk.Button(menu_window, text="Show my cash", command=self.show_cash)
         show_cash_button.pack()
 
-        withdraw_button = tk.Button(menu_window, text="Withdraw money", command=self.withdraw_gui)
+        withdraw_button = tk.Button(menu_window, text="Withdraw money", command=self.withdraw_money)
         withdraw_button.pack()
 
-        deposit_button = tk.Button(menu_window, text="Deposit money", command=self.deposit_gui)
+        deposit_button = tk.Button(menu_window, text="Deposit money", command=self.deposit_money)
         deposit_button.pack()
 
-        send_money_button = tk.Button(menu_window, text="Send money", command=self.send_gui)
+        send_money_button = tk.Button(menu_window, text="Send money", command=self.send_money)
         send_money_button.pack()
 
         exit_button = tk.Button(menu_window, text="Exit", command=self.window.destroy)
@@ -62,6 +53,19 @@ class BankTeller:
 
     def show_cash(self):
         messagebox.showinfo("Balance", f"Your current balance is: {self.cash} $")
+
+    def withdraw_money(self):
+        withdraw_window = tk.Toplevel(self.window)
+        withdraw_window.title("Withdraw Money")
+
+        amount_label = tk.Label(withdraw_window, text="Enter the amount to withdraw:")
+        amount_label.pack()
+
+        amount_entry = tk.Entry(withdraw_window)
+        amount_entry.pack()
+
+        confirm_button = tk.Button(withdraw_window, text="Confirm", command=lambda: self.withdraw(amount_entry.get(), withdraw_window))
+        confirm_button.pack()
 
     def withdraw(self, amount, window):
         try:
@@ -76,44 +80,7 @@ class BankTeller:
 
         window.destroy()
 
-    def deposit(self, amount, window):
-        try:
-            amount = float(amount)
-            self.cash += amount
-            messagebox.showinfo("Deposit", f"Deposited {amount} $. New balance: {self.cash} $")
-        except ValueError:
-            messagebox.showerror("Error", "Invalid amount.")
-
-        window.destroy()
-
-    def send(self, receiver, amount, window):
-        try:
-            receiver = int(receiver)
-            amount = float(amount)
-            if amount <= self.cash:
-                self.cash -= amount
-                messagebox.showinfo("Send Money", f"Money sent successfully. New balance: {self.cash} $")
-            else:
-                messagebox.showerror("Error", "Insufficient funds.")
-        except ValueError:
-            messagebox.showerror("Error", "Invalid input.")
-
-        window.destroy()
-
-    def withdraw_gui(self):
-        withdraw_window = tk.Toplevel(self.window)
-        withdraw_window.title("Withdraw Money")
-
-        amount_label = tk.Label(withdraw_window, text="Enter the amount to withdraw:")
-        amount_label.pack()
-
-        amount_entry = tk.Entry(withdraw_window)
-        amount_entry.pack()
-
-        confirm_button = tk.Button(withdraw_window, text="Confirm", command=lambda: self.withdraw(amount_entry.get(), withdraw_window))
-        confirm_button.pack()
-
-    def deposit_gui(self):
+    def deposit_money(self):
         deposit_window = tk.Toplevel(self.window)
         deposit_window.title("Deposit Money")
 
@@ -126,7 +93,17 @@ class BankTeller:
         confirm_button = tk.Button(deposit_window, text="Confirm", command=lambda: self.deposit(amount_entry.get(), deposit_window))
         confirm_button.pack()
 
-    def send_gui(self):
+    def deposit(self, amount, window):
+        try:
+            amount = float(amount)
+            self.cash += amount
+            messagebox.showinfo("Deposit", f"Deposited {amount} $. New balance: {self.cash} $")
+        except ValueError:
+            messagebox.showerror("Error", "Invalid amount.")
+
+        window.destroy()
+
+    def send_money(self):
         send_window = tk.Toplevel(self.window)
         send_window.title("Send Money")
 
@@ -145,6 +122,20 @@ class BankTeller:
         confirm_button = tk.Button(send_window, text="Confirm", command=lambda: self.send(receiver_entry.get(), amount_entry.get(), send_window))
         confirm_button.pack()
 
+    def send(self, receiver, amount, window):
+        try:
+            receiver = int(receiver)
+            amount = float(amount)
+            if amount <= self.cash:
+                self.cash -= amount
+                messagebox.showinfo("Send Money", f"Money sent successfully. New balance: {self.cash} $")
+            else:
+                messagebox.showerror("Error", "Insufficient funds.")
+        except ValueError:
+            messagebox.showerror("Error", "Invalid input.")
+
+        window.destroy()
+
 if __name__ == "__main__":
-    bank_teller = BankTeller()
+    bank_teller_gui = BankTellerGUI()
 
