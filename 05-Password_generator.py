2609ce516d2728a875298db4bcfad4272fd70ad3
Alejandro Vives <alejandrovt1996@gmail.com>
2024-07-06 22:04:24 +0200

    password label

--- 05-Password_generator.py
+++ 05-Password_generator.py
@@ -1,36 +1,38 @@
 """
-This program will generate a random password between 8 and 16 characters. The user can choose the options they want to include, 
+This program will generate a random password between 8 and 24 characters. The user can choose the options they want to include, 
 such as digits, uppercase letters, lowercase letters, and special characters, as well as the length.
 """
 import tkinter as tk
 import random
 
 def build_password():
-    length = int(length_var.get())
-    final = ""
-    if length < 8 or length > 16:
-        error_label.config(text="Error: Password length must be between 8 and 16.")
+  length = int(length_var.get())
+  final = ""
+  if length < 8 or length > 24:
+    error_label.config(text="Error: Password length must be between 8 and 24.") 
+  else:
+    r_lower = lower_var.get()
+    r_upper = upper_var.get()
+    r_digit = digit_var.get()
+    r_special = special_var.get()
+    char_pool = ""
+    if r_lower:
+      char_pool += "abcdefghijklmnopqrstuvwxyz"
+    if r_upper:
+      char_pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if r_digit:
+      char_pool += "0123456789"
+    if r_special:
+      char_pool += "!@#$%^&*()_+"
+    if len(char_pool) == 0:
+      error_label.config(text="Error: You must select at least one type of character.")
     else:
-        r_lower = lower_var.get()
-        r_upper = upper_var.get()
-        r_digit = digit_var.get()
-        r_special = special_var.get()
-        char_pool = ""
-        if r_lower:
-            char_pool += "abcdefghijklmnopqrstuvwxyz"
-        if r_upper:
-            char_pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        if r_digit:
-            char_pool += "0123456789"
-        if r_special:
-            char_pool += "!@#$%^&*()_+"
-        if len(char_pool) == 0:
-            error_label.config(text="Error: You must select at least one type of character.")
-        else:
-            for i in range(length):
-                final += random.choice(char_pool)
-            password_label.config(text=final)
-            error_label.config(text="")
+      for i in range(length):
+        final += random.choice(char_pool)
+      password_entry.delete(0, tk.END)  # Clear previous password
+      password_entry.insert(0, final)
+      error_label.config(text="")
+      # print(final)
 
 # GUI
 window = tk.Tk()
@@ -46,14 +48,13 @@ y = int((screen_height - window.winfo_screenmmheight()) / 2)
 window.geometry("+{}+{}".format(x, y))
 
 # Password length
-options = ["8", "9", "10", "11", "12", "13", "14", "15", "16"]
+options = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
 length_var = tk.StringVar(window)
-length_var.set(options[0])  
-length_label = tk.Label(window, text="Password length (8-16):")
+length_var.set(options[0])
+length_label = tk.Label(window, text="Password length (8-24):")
 length_label.pack()
 length_menu = tk.OptionMenu(window, length_var, *options)
 length_menu.pack()
-
 
 # Lowercase characters
 lower_label = tk.Label(window, text="Include lowercase characters:")
@@ -87,11 +88,9 @@ special_check.pack()
 generate_button = tk.Button(window, text="Generate Password", command=build_password)
 generate_button.pack(pady=10)
 
-# Password display label
-password_label = tk.Label(window, text="")
-password_label.pack(pady=10)
-
-# password_label.pack()
+# Password display entry
+password_entry = tk.Entry(window, width=50)
+password_entry.pack(pady=10)
 
 # Error label
 error_label = tk.Label(window, text="", fg="red")
